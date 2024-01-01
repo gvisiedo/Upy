5680d08f208337dbe702b845f2e8cf26c527637a
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2024-01-02 00:04:11 +0100

    .

---
+++ 01_EDatos.py
@@ -0,0 +1,36 @@
+lista =[3, 7, 9]
+lista = [3, 'hola', True]
+
+print(lista[2])
+
+print(lista[:-1])
+
+lista.index(True)
+
+lista.insert(1, 'adios mundo')
+
+print(lista)
+lista.append(3)
+
+print(lista)
+
+lista.reverse()
+
+
+tupla = (4,5,6, 'Hola mundo')
+print(tupla)
+
+print(tupla[3])
+
+print(tupla[1:2])
+print(tupla.index(4))
+
+diccionario = {
+    'Clave1': 'Hola mundo',
+    'clave':3,
+    3:True
+}
+
+print(diccionario[2])
+
+
