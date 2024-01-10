1c4732f5ba2693333d498b9832d07e08dc63300d
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2024-01-10 18:52:38 +0100

    .

--- 04_def.py
+++ 04_def.py
@@ -24,3 +24,22 @@ def funcion3(**args):
         print(argument, "=>", args[argument])
 
 funcion3(arg1=1, arg2="holamundo")
+
+def devuleveVariosValores():
+    return 1,2,"Hola mundo"
+devuleveVariosValores()
+x,y,w = devuleveVariosValores()
+
+def cuadrado(numero):
+    """
+    Esta función nos permite elevar al cuadrado un numero
+    argumentos:
+    numero de tipo numero
+
+    :param numero:
+    :return:
+    """
+    return numero**2
+
+help(cuadrado)
+
