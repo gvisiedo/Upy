cfc44ebacdd18e3972cdd4edbebdc9524899fd3f
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2024-01-17 18:06:02 +0100

    .

---
+++ 05_defincluded.py
@@ -0,0 +1,58 @@
+import math
+import sys
+import os
+import os.path
+import random
+
+math.fabs(-150.36)
+
+math.gcd(30,12)
+
+math.floor(60.30)
+
+math.ceil(60.30)
+
+math.factorial(5)
+
+math.trunc(60)
+
+math.sin(math.pi/4)
+
+math.cos(math.pi/4)
+
+math.tan(math.pi/2)
+
+math.asin(1)
+
+math.acos(1)
+
+math.atan(1)
+
+math.hypot(12, 23)
+
+math.log(12)
+
+math.log2(12)
+
+math.log10(12)
+
+math.pow(45, 5)
+
+math.sqrt(25)
+
+math.pi
+
+os.getcwd()
+
+os.path.abspath("")
+
+random.randint(5, 9)
+
+random.choice(['hola',3,5,True])
+
+
+
+
+
+
+
